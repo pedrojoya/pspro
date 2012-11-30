197e7501d09e5029079942a26304bdb459b167dd
Pedro Joya <pedrojoya@gmail.com>
2012-11-30 14:28:04 +0100

    J7CC0205 Terminado

---
+++ J7CC0204-Lock/src/es/iessaladillo/pedrojoya/pspro/j7cc0204/main/Main.java
@@ -0,0 +1,19 @@
+package es.iessaladillo.pedrojoya.pspro.j7cc0204.main;
+
+import es.iessaladillo.pedrojoya.pspro.j7cc0204.tarea.Trabajo;
+import es.iessaladillo.pedrojoya.pspro.j7cc0204.tarea.Impresora;
+
+public class Main {
+
+	public static void main (String args[]){
+		// Creo una nueva impresora.
+		Impresora impresora = new Impresora();
+		// Creo e inicio 10 hilos que ejecutan 10 trabajos.
+		Thread hilos[]= new Thread[10];
+		for (int i = 0; i < 10; i++){
+			hilos[i] = new Thread(new Trabajo(impresora), "Trabajo " + i);
+			hilos[i].start();
+		}
+	}
+
+}
---
+++ J7CC0205-ReadWriteLock/src/es/iessaladillo/pedrojoya/pspro/j7cc0205/main/Main.java
@@ -0,0 +1,43 @@
+package es.iessaladillo.pedrojoya.pspro.j7cc0205.main;
+
+import es.iessaladillo.pedrojoya.pspro.j7cc0205.tarea.Producto;
+import es.iessaladillo.pedrojoya.pspro.j7cc0205.tarea.Cliente;
+import es.iessaladillo.pedrojoya.pspro.j7cc0205.tarea.Tienda;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Creo el producto con un precio inicial de 100.
+		Producto producto = new Producto(100);
+		// Voy a crear 4 hilos clientes que consultarán el mismo producto.
+		Thread hilosClientes[]= new Thread[4];
+		// Creo e inicio 1 hilo para la tienda
+		Thread hiloTienda = new Thread(new Tienda(producto), "Tienda");
+		hiloTienda.start();
+		// Espero 1 segundo para crear los clientes.
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Creo e inicio los dos primeros hilos clientes.
+		for (int i = 0; i < 2; i++){
+			hilosClientes[i] = new Thread(new Cliente(producto), "Cliente " + i);
+			hilosClientes[i].start();
+		}
+		// Espero 7 segundos para crear el resto de los clientes.
+		try {
+			Thread.sleep(7000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Creo e inicio los dos hilos clientes restantes.
+		for (int i = 2; i < 4; i++){
+			hilosClientes[i] = new Thread(new Cliente(producto), "Cliente " + i);
+			hilosClientes[i].start();
+		}		
+	}
+
+}
