196d56825d922da5216a7508d066023c3c6b33ff
Pedro Joya <pedrojoya@gmail.com>
2012-12-02 17:11:24 +0100

    Cambios estéticos

--- J7CC0204-Lock/src/es/iessaladillo/pedrojoya/pspro/j7cc0204/tarea/Trabajo.java
+++ J7CC0204-Lock/src/es/iessaladillo/pedrojoya/pspro/j7cc0204/tarea/Trabajo.java
@@ -3,16 +3,16 @@ package es.iessaladillo.pedrojoya.pspro.j7cc0204.tarea;
 // Trabajo que envía un documento a la impresora.
 public class Trabajo implements Runnable {
 
-	// Propiedades.
-	private Impresora impresora;
-	
-	// Constructor
-	public Trabajo(Impresora impresora){
-		this.impresora = impresora;
-	}
-	
-	@Override
-	public void run() {
-		impresora.imprimir(new Object());
-	}
+    // Propiedades.
+    private Impresora impresora;
+    
+    // Constructor
+    public Trabajo(Impresora impresora){
+        this.impresora = impresora;
+    }
+    
+    @Override
+    public void run() {
+        impresora.imprimir(new Object());
+    }
 }
