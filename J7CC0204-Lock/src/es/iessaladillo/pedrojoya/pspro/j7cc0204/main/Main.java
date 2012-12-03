281848447d01d03faac318e47d3d67f25300a2a1
Pedro Joya <pedrojoya@gmail.com>
2012-12-03 18:37:27 +0100

    Revert "J7CC0204 -J7CC0206 Terminados y arreglados"
    This reverts commit f5567e134157afba53fed5ddf3fbaf113ffdb443.
    
    Conflicts:
    	J7CC0204-Lock/src/es/iessaladillo/pedrojoya/pspro/j7cc0204/main/Main.java
    	J7CC0205-ReadWriteLock/src/es/iessaladillo/pedrojoya/pspro/j7cc0205/main/Main.java
    	J7CC0206-LockJusto/.project

--- J7CC0204-Lock/src/es/iessaladillo/pedrojoya/pspro/j7cc0204/main/Main.java
+++ J7CC0204-Lock/src/es/iessaladillo/pedrojoya/pspro/j7cc0204/main/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package es.iessaladillo.pedrojoya.pspro.j7cc0204.main;
 
 import es.iessaladillo.pedrojoya.pspro.j7cc0204.tarea.Trabajo;
@@ -17,3 +18,24 @@ public class Main {
     }
 
 }
+=======
+package es.iessaladillo.pedrojoya.pspro.j7cc0204.main;
+
+import es.iessaladillo.pedrojoya.pspro.j7cc0204.tarea.Trabajo;
+import es.iessaladillo.pedrojoya.pspro.j7cc0204.tarea.Impresora;
+
+public class Main {
+
+    public static void main (String args[]){
+        // Creo una nueva impresora.
+        Impresora impresora = new Impresora();
+        // Creo e inicio 10 hilos que ejecutan 10 trabajos.
+        Thread hilos[]= new Thread[10];
+        for (int i = 0; i < 10; i++){
+            hilos[i] = new Thread(new Trabajo(impresora), "Trabajo " + i);
+            hilos[i].start();
+        }
+    }
+
+}
+>>>>>>> parent of f5567e1... J7CC0204 -J7CC0206 Terminados y arreglados
