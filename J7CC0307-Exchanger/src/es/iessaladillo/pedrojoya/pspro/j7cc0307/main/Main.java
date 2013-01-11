2a53caf24611a81c0d3d83df8058d94ec884c65f
Pedro Joya <pedrojoya@gmail.com>
2013-01-11 21:55:50 +0100

    J7CC0307 modificaciones

--- J7CC0307-Exchanger/src/es/iessaladillo/pedrojoya/pspro/j7cc0307/main/Main.java
+++ J7CC0307-Exchanger/src/es/iessaladillo/pedrojoya/pspro/j7cc0307/main/Main.java
@@ -8,18 +8,18 @@ import java.util.concurrent.Exchanger;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// Buffers: estructuras de datos que serán intercambiadas.
-		List<String> buffer1 = new ArrayList<>();
-		List<String> buffer2 = new ArrayList<>();
-		// Objeto Exchanger intercambiador de ese tipo de estructuras de datos.
-		// que compartirán tanto Productor como Consumidor.
-		Exchanger<List<String>> intercambiador = new Exchanger<>();
-		// Creo e inicio los hilos del productor y del consumidor.
-		Thread hiloProductor = new Thread(new Productor(buffer1, intercambiador));
-		Thread hiloConsumidor = new Thread(new Consumidor(buffer2, intercambiador));
-		hiloProductor.start();
-		hiloConsumidor.start();
-	}
+    public static void main(String[] args) {
+        // Buffers: estructuras de datos que serán intercambiadas.
+        List<String> buffer1 = new ArrayList<>();
+        List<String> buffer2 = new ArrayList<>();
+        // Objeto Exchanger intercambiador de ese tipo de estructuras de datos.
+        // que compartirán tanto Productor como Consumidor.
+        Exchanger<List<String>> intercambiador = new Exchanger<>();
+        // Creo e inicio los hilos del productor y del consumidor.
+        Thread hiloProductor = new Thread(new Productor(buffer1, intercambiador));
+        Thread hiloConsumidor = new Thread(new Consumidor(buffer2, intercambiador));
+        hiloProductor.start();
+        hiloConsumidor.start();
+    }
 
 }
