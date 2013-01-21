19a35f94fd37b1829141938b1e731a9f6b4006fc
Pedro Joya <pedrojoya@gmail.com>
2013-01-21 08:52:06 +0100

    JCC0405

---
+++ J7CC0405-InvokeAll/src/es/iessaladillo/pedrojoya/pspro/j7cc0405/tarea/Resultado.java
@@ -0,0 +1,30 @@
+package es.iessaladillo.pedrojoya.pspro.j7cc0405.tarea;
+
+// Encapsula el resultado de una tarea.
+public class Resultado {
+
+    private String nombre;
+    private int valor;
+
+    Resultado(String nombre, int valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+}
