b728a4a5d3e5f48ea7109fe2ca2dac6df572455c
Pedro Joya <pedrojoya@gmail.com>
2013-01-24 20:21:14 +0100

    J7CC0410

---
+++ J7CC0410-CompletionService/src/es/iessaladillo/pedrojoya/pspro/j7cc0410/tarea/Solicitante.java
@@ -0,0 +1,29 @@
+package es.iessaladillo.pedrojoya.pspro.j7cc0410.tarea;
+
+import java.util.concurrent.CompletionService;
+
+// Simula un solicitante de un informe
+public class Solicitante implements Runnable {
+
+    private String nombre;
+
+    // CompletionService usado para la ejecución de las tareas de
+    // generación de informes
+    private CompletionService<String> servicio;
+
+    // Constructor. Recibe el nombre del solicitante y el servicio.
+    public Solicitante(String nombre, CompletionService<String> servicio) {
+        this.nombre = nombre;
+        this.servicio = servicio;
+    }
+
+    @Override
+    public void run() {
+        // Creo el generador de informes y lo envío al servicio.
+        String titulo = "Historial";
+        Generador generador = new Generador(nombre, titulo);
+        System.out.printf("Solicitante -> %s solicita %s\n", nombre, titulo);
+        servicio.submit(generador);
+    }
+
+}
