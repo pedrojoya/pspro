6a145d1cd6bcf72f2a1a71184b1c2572e9ae3b83
pedrojoya <pedrojoya@gmail.com>
2013-09-27 21:46:01 +0200

    GAR0103 mejorado

--- GAR0103/src/Main.java
+++ GAR0103/src/Main.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 
 public class Main {
 
@@ -10,18 +12,24 @@ public class Main {
         Runtime entorno = Runtime.getRuntime();
         String orden = "java Lorito";
         try {
-            // Se ejecuta el proceso Lorito.
-            Process proceso = entorno.exec(orden);
-            // Se escribe el texto que recibirá Lorito.
+            // Se ejecuta el proceso Lorito estableciendo como directorio de
+            // trabajo donde esté Lorito.class (relativo al directorio del
+            // proyecto)
+            Process proceso = entorno.exec(orden, null, new File("bin"));
+            // Se informa al usuario que va a comenzar el envío.
             System.out.println("Enviando a Lorito...");
+            // Se envía el mensaje al programa Lorito.
             OutputStream salida = proceso.getOutputStream();
             String mensaje = "No te digo trigo por no llamarte Rodrigo\n";
-            mensaje += "La cosa está muy mala\n";
-            System.out.print(mensaje);
-            salida.write(mensaje.getBytes());
-            salida.flush();
-            // Leo del flujo proveniente del subproceso.
+            mensaje += "La cosa está muy mala";
+            PrintWriter escritor = new PrintWriter(salida);
+            escritor.println(mensaje);
+            escritor.close();
+            // Se informa al usuario del mensaje enviado y que comienza la
+            // lectura de la respuesta.
+            System.out.println(mensaje);
             System.out.println("\nRecibido desde Lorito...");
+            // Se lee del flujo proveniente del subproceso.
             BufferedReader lector = new BufferedReader(new InputStreamReader(
                     proceso.getInputStream()));
             String linea = lector.readLine();
