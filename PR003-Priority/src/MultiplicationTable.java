a1f7de947aae493c64ab9ff43c18cb1178ff9a40
pedrojoya <pedrojoya@gmail.com>
2018-11-29 13:44:00 +0100

    PR003 - Initial commit

---
+++ PR003-Priority/src/MultiplicationTable.java
@@ -0,0 +1,17 @@
+class MultiplicationTable implements Runnable {
+
+    private final int number;
+
+    MultiplicationTable(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%s: %d * %d = %d\n", Thread.currentThread()
+                    .getName(), number, i, i * number);
+        }
+    }
+
+}
