622ac552567b74ad6b0b95c889a380285df5d12e
pedrojoya <pedrojoya@gmail.com>
2018-11-29 21:08:30 +0100

    PR009 - Initial commit

---
+++ PR009-SynchronizedMethod/src/Account.java
@@ -0,0 +1,25 @@
+public class Account {
+
+    private float balance;
+
+    public Account(float initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    // Try and remove synchronized keyword and see what happens.
+    public synchronized void deposit(float amount) {
+        balance += amount;
+        System.out.print(".");
+    }
+
+    // Try and remove synchronized keyword and see what happens.
+    public synchronized void debit(int amount) {
+        balance -= amount;
+        System.out.print(".");
+    }
+
+}
---
+++ PR009-SynchronizedMethod/src/AccountConsumer.java
@@ -0,0 +1,16 @@
+public class AccountConsumer implements Runnable {
+
+    private final Account account;
+
+    public AccountConsumer(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            account.debit(5);
+        }
+    }
+
+}
