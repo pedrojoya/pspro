1f1f5ba61d699247380c1bc8503149f8e664bc6a
pedrojoya <pedrojoya@gmail.com>
2018-12-01 22:36:56 +0100

    PR014 - Initial commit

---
+++ PR014-Condition/src/Bakery.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bakery {
+
+    private static final int TRAY_CAPACITY = 10;
+
+    private final ArrayList<Integer> tray = new ArrayList<>();
+    private final ReentrantLock lock = new ReentrantLock(true);
+    private final Condition isNotFull = lock.newCondition();
+    private final Condition isNotEmpty = lock.newCondition();
+
+    public void addToTray(Integer doughnut) throws InterruptedException {
+        lock.lock();
+        try {
+            while (tray.size() >= TRAY_CAPACITY) {
+                System.out.println("Producer waiting for the tray to have room");
+                isNotFull.await();
+            }
+            tray.add(doughnut);
+            System.out.printf("Producer puts doughnut #%d on the tray\n", doughnut);
+            isNotEmpty.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Integer extractFromTray() throws InterruptedException {
+        Integer doughnut;
+        lock.lock();
+        try {
+            while (tray.isEmpty()) {
+                System.out.println("Consumer waiting for the tray to have a doughnut");
+                isNotEmpty.await();
+            }
+            doughnut = tray.remove(0);
+            System.out.printf("Consumer extracts doughnut #%d from tray\n", doughnut);
+            isNotFull.signalAll();
+            return doughnut;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
