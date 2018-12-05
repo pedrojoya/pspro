93a637647844fd5c44bf64c2fd7a6850a920f13b
pedrojoya <pedrojoya@gmail.com>
2018-12-05 17:59:29 +0100

    PR023 - Initial commit

---
+++ PR023-DoubleAdder/src/Account.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.atomic.DoubleAdder;
+
+public class Account {
+
+    private final DoubleAdder balance = new DoubleAdder();
+
+    public Account(double initialBalance) {
+        balance.add(initialBalance);
+    }
+
+    public double getBalance() {
+        return balance.doubleValue();
+    }
+
+    public void deposit(long amount) {
+        balance.add(amount);
+        System.out.print(".");
+    }
+
+    public void debit(long amount) {
+        balance.add(-amount);
+        System.out.print(".");
+    }
+
+}
---
+++ PR023-DoubleAdder/src/Main.java
@@ -0,0 +1,25 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        Account account = new Account(0);
+        // Show initial balance.
+        System.out.printf("%s -> Initial balance: %.2f€\n",
+                simpleDateFormat.format(new Date()), account.getBalance());
+        // Start both saver and consumer threads. Both threads share the same account.
+        Thread saverThread = new Thread(new AccountSaver(account));
+        saverThread.start();
+        Thread consumerThread = new Thread(new AccountConsumer(account));
+        consumerThread.start();
+        // Wait for both threads to finish.
+        saverThread.join();
+        consumerThread.join();
+        // Show final balance.
+        System.out.printf("\n%s -> Final balance: %.2f€\n",
+                simpleDateFormat.format(new Date()), account.getBalance());
+    }
+
+}
