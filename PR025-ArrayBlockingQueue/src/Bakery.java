086756faa04afe44c00c0d1d3bd3fef366dd9b57
pedrojoya <pedrojoya@gmail.com>
2018-12-07 17:55:18 +0100

    PR025 - Initial commit

---
+++ PR025-ArrayBlockingQueue/src/Bakery.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Bakery {
+
+    private static final int TRAY_CAPACITY = 10;
+
+    private final ArrayBlockingQueue<Integer> tray = new ArrayBlockingQueue<>(TRAY_CAPACITY);
+
+    public void addToTray(Integer doughnut) throws InterruptedException {
+        tray.put(doughnut);
+        System.out.printf("Producer puts doughnut #%d on the tray\n", doughnut);
+    }
+
+    public Integer extractFromTray() throws InterruptedException {
+        Integer doughnut = tray.take();
+        System.out.printf("Consumer extracts doughnut #%d from tray\n", doughnut);
+        return doughnut;
+    }
+
+}
