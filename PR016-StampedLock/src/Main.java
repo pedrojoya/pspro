23906a9b7d810e269b9f64c53219a7333cec5414
pedrojoya <pedrojoya@gmail.com>
2020-04-21 11:36:54 +0200

    PR016-StampedLock - Use Java 8 time api

--- PR016-StampedLock/src/Main.java
+++ PR016-StampedLock/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         Product product = new Product(100.00);
-        Thread clientThreads[]= new Thread[4];
+        Thread[] clientThreads = new Thread[4];
         for (int i = 0; i < 4; i++) {
             clientThreads[i] = new Thread(new Client(product), "Client " + i);
         }
@@ -12,16 +12,16 @@ public class Main {
         shopThread.start();
         // Wait to start some clients.
         TimeUnit.SECONDS.sleep(1);
-        for (int i = 0; i < 2; i++){
+        for (int i = 0; i < 2; i++) {
             clientThreads[i].start();
         }
         // Wait to start the rest of the clients.
         TimeUnit.SECONDS.sleep(3);
-        for (int i = 2; i < 4; i++){
+        for (int i = 2; i < 4; i++) {
             clientThreads[i].start();
         }
-        // Try and check a client thread blocks the shop thread but not other clients threads.
-        // Try and check the shop thread blocks client threads.
+        // Try to check a client thread blocks the shop thread but not other clients threads.
+        // Try to check the shop thread blocks client threads.
     }
 
 }
--- PR016-StampedLock/src/Product.java
+++ PR016-StampedLock/src/Product.java
@@ -1,12 +1,12 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.StampedLock;
 
 public class Product {
 
     private double price;
-    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
     private final StampedLock stampedLock = new StampedLock();
 
     public Product(double initialPrice) {
@@ -20,7 +20,7 @@ public class Product {
 
     private double consultPrice(long stamp) throws InterruptedException {
         System.out.printf("%s -> %s - Consulting price...\n",
-                simpleDateFormat.format(new Date()),
+                LocalTime.now().format(dateTimeFormatter),
                 Thread.currentThread().getName());
         TimeUnit.SECONDS.sleep(3);
         double value = price;
@@ -33,7 +33,7 @@ public class Product {
             }
         }
         System.out.printf("%s -> %s - Price: %.2f\n",
-                simpleDateFormat.format(new Date()),
+                LocalTime.now().format(dateTimeFormatter),
                 Thread.currentThread().getName(),
                 value);
         return value;
@@ -50,12 +50,12 @@ public class Product {
 
     private void incrementPrice(double increment) throws InterruptedException {
         System.out.printf("%s -> %s - Updating price...\n",
-                simpleDateFormat.format(new Date()),
+                LocalTime.now().format(dateTimeFormatter),
                 Thread.currentThread().getName());
         TimeUnit.SECONDS.sleep(1);
         this.price += increment;
         System.out.printf("%s -> %s - New price: %.2f\n",
-                simpleDateFormat.format(new Date()),
+                LocalTime.now().format(dateTimeFormatter),
                 Thread.currentThread().getName(),
                 this.price);
     }
