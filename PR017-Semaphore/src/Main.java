2b1a48c583d095360d60b7016fc69bfdb732eddd
pedrojoya <pedrojoya@gmail.com>
2020-04-21 12:08:13 +0200

    PR017-Semaphore - Use Java 8 time api

--- PR017-Semaphore/src/Main.java
+++ PR017-Semaphore/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
-    public static void main (String args[]){
+    public static void main(String[] args) {
         PrintingQueue printingQueue = new PrintingQueue(3);
-        Thread printJobThreads[]= new Thread[10];
+        Thread[] printJobThreads = new Thread[10];
         for (int i = 0; i < 10; i++) {
             printJobThreads[i] = new Thread(new PrintJob(printingQueue, "Document #" + i), "Print job #" + i);
         }
--- PR017-Semaphore/src/Printer.java
+++ PR017-Semaphore/src/Printer.java
@@ -1,12 +1,12 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Printer {
 
     private final int printerNumber;
-    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
     private final Random random = new Random();
 
     public Printer(int printerNumber) {
@@ -15,12 +15,15 @@ public class Printer {
 
     public void printDocument(String document) throws InterruptedException {
         System.out.printf("%s -> %s: Document printing started on printer %d\n",
-                simpleDateFormat.format(new Date()), Thread.currentThread().getName(), printerNumber);
+                LocalTime.now().format(dateTimeFormatter),
+                Thread.currentThread().getName(), printerNumber);
         System.out.printf("%s -> %s: %s...\n",
-                simpleDateFormat.format(new Date()), Thread.currentThread().getName(), document);
+                LocalTime.now().format(dateTimeFormatter),
+                Thread.currentThread().getName(), document);
         TimeUnit.SECONDS.sleep(random.nextInt(5));
         System.out.printf("%s -> %s: Printing finished on printer %d\n",
-                simpleDateFormat.format(new Date()), Thread.currentThread().getName(), printerNumber);
+                LocalTime.now().format(dateTimeFormatter),
+                Thread.currentThread().getName(), printerNumber);
     }
 
 }
