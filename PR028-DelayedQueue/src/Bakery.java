c9ce13b3481e0b759b0dc00a1273d88c9006a425
pedrojoya <pedrojoya@gmail.com>
2020-04-22 08:54:01 +0200

    PR028-DelayedQueue - Use Java 8 time api

--- PR028-DelayedQueue/src/Bakery.java
+++ PR028-DelayedQueue/src/Bakery.java
@@ -1,22 +1,24 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.DelayQueue;
 
 public class Bakery {
 
-    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+    final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
     private final DelayQueue<Doughnut> tray = new DelayQueue<>();
 
     public void addToTray(Doughnut doughnut) {
         tray.put(doughnut);
-        System.out.printf("%s -> Producer puts doughnut #%d on the tray\n", simpleDateFormat.format(new Date()), doughnut.getNumber());
+        System.out.printf("%s -> Producer puts doughnut #%d on the tray\n",
+                LocalTime.now().format(dateTimeFormatter), doughnut.getNumber());
     }
 
     public Doughnut extractFromTray() throws InterruptedException {
-        System.out.printf("%s -> Consumer tries to extract a doughnut from tray\n", simpleDateFormat.format(new Date()));
+        System.out.printf("%s -> Consumer tries to extract a doughnut from tray\n",
+                LocalTime.now().format(dateTimeFormatter));
         Doughnut doughnut = tray.take();
-        System.out.printf("%s -> Consumer extracts doughnut #%d from tray\n", simpleDateFormat.format(new Date()), doughnut.getNumber());
+        System.out.printf("%s -> Consumer extracts doughnut #%d from tray\n",
+                LocalTime.now().format(dateTimeFormatter), doughnut.getNumber());
         return doughnut;
     }
 
